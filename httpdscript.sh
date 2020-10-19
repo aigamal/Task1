29e0e75834567ed098eb1a40a06dd514c96869b0
aigamal <33626347+aigamal@users.noreply.github.com>
2020-10-19 12:41:46 +0400

    Update httpdscript.sh

--- httpdscript.sh
+++ httpdscript.sh
@@ -3,6 +3,6 @@ sudo su
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install apache2
-wget https://raw.githubusercontent.com/trilogy-group/sre-rwa/master/task1/index.html -O /var/www/html/index.html
+wget https://github.com/aigamal/Task1/raw/main/index.html -O /var/www/html/index.html
 sudo systemctl enable apache2
 sudo systemctl restart apache2
