6ef0cbb2e77158a4686f26312bc53c11e1ff0844
aigamal <33626347+aigamal@users.noreply.github.com>
2020-10-19 14:10:26 +0400

    Delete httpdscript.sh

--- httpdscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo su
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install apache2
-wget https://github.com/aigamal/Task1/raw/main/index.html -O /var/www/html/index.html
-sudo systemctl enable apache2
-sudo systemctl restart apache2
